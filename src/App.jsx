68067cccf5fa1af81295455e35abd2a7b6bcaf5a
Kim Gustafsson <kim.gustafsson@tretton37.com>
2022-03-29 09:54:54 +0200

    Add a scene and a box

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import logo from './logo.svg';
 import './App.css';
 
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
+
 function App() {
-  const [count, setCount] = useState(0);
+  const cameraPosition = 6;
 
   return (
     <div className='App'>
@@ -11,6 +36,23 @@ function App() {
         <img src={logo} className='App-logo' alt='logo' />
         <h5 className='App-title'>Three.js Journey with react-three-fiber</h5>
       </header>
+      <section>
+        <Canvas
+          shadows
+          dpr={[1, 2]}
+          camera={{
+            fov: 55,
+            near: 0.1,
+            far: 1000,
+            position: [cameraPosition, cameraPosition, cameraPosition],
+          }}
+        >
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <Box position={[-1.2, 0, 0]} />
+          <Box position={[1.2, 0, 0]} />
+        </Canvas>
+      </section>
     </div>
   );
 }
