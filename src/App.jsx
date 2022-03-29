01c034454bc40561572d316244ff48add2a08c7e
Kim Gustafsson <kim.gustafsson@tretton37.com>
2022-03-29 10:03:40 +0200

    Fix tab text and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function Box(props) {
-  // This reference will give us direct access to the mesh
   const mesh = useRef();
-  // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
-  // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
       {...props}
